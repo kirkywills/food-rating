1d223c19bc0615c0eb11e4ee72bc68312e691e05
Kirk Williams <kirkywills@gmail.com>
2020-10-26 17:13:28 -0400

    first commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout>
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Food Rating
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <q-page class="q-pa-lg">
+  	<div class="row q-gutter-lg">
+
+			<food
+				v-for="food in foods"
+				:key="food.id"
+				:food="food" />
+
+	    <button-add
+	    	@click="showAddFoodModal = true" />
+
+	    <q-dialog
+	    	v-model="showAddFoodModal">
+        <modal-add-edit-food
+        	@close="showAddFoodModal = false"
+        	type="add" />
+      </q-dialog>
+
+  	</div>
+  </q-page>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex'
+
+	export default {
+	  data() {
+	  	return {
+	  		showAddFoodModal: false
+	  	}
+	  },
+	  computed: {
+	  	...mapGetters('foods', ['foods'])
+	  },
+	  components: {
+	  	'food' : require('components/Food.vue').default,
+	  	'button-add' : require('components/ButtonAdd.vue').default,
+	  	'modal-add-edit-food' : require('components/ModalAddEditFood.vue').default
+	  }
+	}
+</script>
